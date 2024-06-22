5a0e215d54e4f3e464e3ad95fe8b02e23e640785
psw9428 <psw9428@naver.com>
2024-06-22 11:14:10 +0900

    Upload_project

---
+++ src/main/java/com/group/gorani/GoraniApplication.java
@@ -0,0 +1,13 @@
+package com.group.gorani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoraniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoraniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/gorani/GoraniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.gorani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoraniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
